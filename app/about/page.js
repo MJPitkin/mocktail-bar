8c42fc44a66e6f2e6c427324e0595feaa367bf19
mjpitkin <mjpitkin@gmail.com>
2024-04-11 15:52:15 +0100

    added routes for main navbar pages, tweaked navbar css to attempt to space it closer to the figma design

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <main className="flex min-w-screen min-h-screen flex-col flex-wrap items-center justify-evenly p-24">
+    <div>
+        <h1>about header</h1>
+        </div>
+    </main>
+  )
+}
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,14 +3,20 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='w-full bg-slate-500 flex justify-around'>
-        <Link href="/" className='hover:text-white'>Home</Link>
-        <Link href="/menu" className='hover:text-white'>Menu</Link>
-        <Link href="/bookings" className='hover:text-white'>Bookings</Link>
-        <Link href="/about" className='hover:text-white'>About Us</Link>
-        <Link href="/eureka" className='hover:text-white'>Eureka's Roti</Link>
-        <Link href="/login" className='hover:text-white'>Login</Link>
-        <Link href="/signup" className='hover:text-white'>Sign Up</Link>
+    <div className='w-full bg-slate-500 flex justify-between'>
+      <h1>Logo</h1>
+        <div className='w-1/2 flex justify-around'>
+        
+        <Link href="/" className='text-black hover:text-white'>Home</Link>
+        <Link href="/menu" className='text-black hover:text-white'>Menu</Link>
+        <Link href="/bookings" className='text-black hover:text-white'>Bookings</Link>
+        <Link href="/about" className='text-black hover:text-white'>About Us</Link>
+        <Link href="/eureka" className='text-black hover:text-white'>Eureka&apos;s Roti</Link>
+        </div>
+        <div className='w-1/6 flex justify-around'>
+        <Link href="/login" className='text-black hover:text-white'>Login</Link>
+        <Link href="/signup" className='text-black hover:text-white'>Sign Up</Link>
+        </div>
     </div>
   )
 }
