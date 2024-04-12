d8d092938aabbbf86880bb186f27e8aeb08b4e55
mjpitkin <mjpitkin@gmail.com>
2024-04-12 17:25:14 +0100

    further tweaks to the navbar, altered background colour of page

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   const greenBawkses = "w-96 h-48 bg-red-500 md:bg-cyan-500 lg:bg-slate-500"
 
   return (
-    <main className="flex min-w-screen min-h-screen flex-col flex-wrap items-center justify-evenly p-24">
+    <main className="flex min-w-screen min-h-screen flex-col flex-wrap items-center justify-evenly p-24 bg-gray-600">
       <div>
         <h1>A Header for Homepage</h1>
       </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='w-full bg-slate-500 flex justify-between'>
+    <div className='w-full h-10 bg-gray-400 flex justify-between border-solid border-black border-b-[1px]'>
       <h1>Logo</h1>
         <div className='w-1/2 flex justify-around'>
         
